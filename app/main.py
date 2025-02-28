9ecd13cbfb2aaa5f5cf6235945f8173381c64e8f
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2025-02-28 00:26:52 -0600

    feat: add routers for board, list, and task management to main application

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from datetime import datetime, timezone, timedelta
+from app.routers import board_router, list_router, task_router
 
 app = FastAPI(title="Kanban API")
 
@@ -8,3 +9,8 @@ app = FastAPI(title="Kanban API")
 def health_check():
     formatted_time = datetime.now(timezone(timedelta(hours=-6))).strftime("%H:%M:%S %Y-%m-%d")
     return {"status": f"ok", "timestamp": formatted_time}
+
+# Routers
+app.include_router(board_router)
+app.include_router(list_router)
+app.include_router(task_router)
